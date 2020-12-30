fa9f8322cd8f51477b374c8c40295196477c0a9d
wonsuckl <wurijang@naver.com>
2020-12-31 03:09:06 +0900

    [feat]change contact way

--- Cherish-Func-Test/FunctionTest/WaterFlow/WaterFlowPopUpVC.swift
+++ Cherish-Func-Test/FunctionTest/WaterFlow/WaterFlowPopUpVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class WaterFlowPopUpVC: UIViewController {
     var ct : FetchedContact?
@@ -15,13 +16,19 @@ class WaterFlowPopUpVC: UIViewController {
             popupView.layer.cornerRadius = 20
         }
     }
+    @IBOutlet weak var firstTagLabel: UILabel!{
+        didSet{
+            firstTagLabel.layer.cornerRadius = 10
+            firstTagLabel.layer.borderWidth = 1
+            firstTagLabel.layer.borderColor = UIColor.blue.cgColor
+        }
+    }
     @IBOutlet weak var callingBtn: UIButton!
     @IBOutlet weak var smsBtn: UIButton!
     @IBOutlet weak var kakaoBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func backToFlow(_ sender: Any) {
@@ -35,31 +42,70 @@ class WaterFlowPopUpVC: UIViewController {
         }
     }
     @IBAction func smsing(_ sender: Any) {
-        guard let pvc = self.presentingViewController else {return}
-        self.dismiss(animated: true) {
-            let storyBoard: UIStoryboard = UIStoryboard(name: "SMS", bundle: nil)
-            if let vc = storyBoard.instantiateViewController(withIdentifier: "SMSVC") as? SMSVC{
-                vc.modalPresentationStyle = .overCurrentContext
-                vc.modalTransitionStyle = .crossDissolve
-                vc.sms_ct = self.ct
-                pvc.present(vc, animated: true, completion: nil)
-            }
+        //        guard let pvc = self.presentingViewController else {return}
+        //        self.dismiss(animated: true) {
+        //            let storyBoard: UIStoryboard = UIStoryboard(name: "SMS", bundle: nil)
+        //            if let vc = storyBoard.instantiateViewController(withIdentifier: "SMSVC") as? SMSVC{
+        //                vc.modalPresentationStyle = .overCurrentContext
+        //                vc.modalTransitionStyle = .crossDissolve
+        //                vc.sms_ct = self.ct
+        //                pvc.present(vc, animated: true, completion: nil)
+        //            }
+        //        }
+        let messageComposer = MFMessageComposeViewController()
+        messageComposer.messageComposeDelegate = self
+        if MFMessageComposeViewController.canSendText(){
+            messageComposer.recipients = ["\(ct?.telephone ?? "0")"]
+            messageComposer.body = ""
+            messageComposer.modalPresentationStyle = .currentContext
+            //messageComposer.modalTransitionStyle = .crossDissolve
+            self.present(messageComposer, animated: true, completion: nil)
         }
         
     }
     
     @IBAction func kakaotalking(_ sender: Any) {
+        let kakaoTalk = "kakaotalk://"
         
-        guard let pvc = self.presentingViewController else {return}
+        let kakaoTalkURL = NSURL(string: kakaoTalk)
         
-        self.dismiss(animated: true) {
-            let storyBoard: UIStoryboard = UIStoryboard(name: "KakaoTemplate", bundle: nil)
-            if let vc = storyBoard.instantiateViewController(withIdentifier: "KakaoTemplateVC") as? KakaoTemplateVC{
-                vc.modalPresentationStyle = .overCurrentContext
-                vc.modalTransitionStyle = .crossDissolve
-                pvc.present(vc, animated: true, completion: nil)
-            }
+        if UIApplication.shared.canOpenURL(kakaoTalkURL! as URL) {
+            
+            UIApplication.shared.openURL(kakaoTalkURL! as URL)
         }
+        else {
+            print("No kakaostory installed.")
+        }
+    }
+}
+//        guard let pvc = self.presentingViewController else {return}
+//        
+//        self.dismiss(animated: true) {
+//            let storyBoard: UIStoryboard = UIStoryboard(name: "KakaoTemplate", bundle: nil)
+//            if let vc = storyBoard.instantiateViewController(withIdentifier: "KakaoTemplateVC") as? KakaoTemplateVC{
+//                vc.modalPresentationStyle = .overCurrentContext
+//                vc.modalTransitionStyle = .crossDissolve
+//                pvc.present(vc, animated: true, completion: nil)
+//            }
+//        }
+
+
+extension WaterFlowPopUpVC: MFMessageComposeViewControllerDelegate{
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch result {
+        case MessageComposeResult.sent:
+            print("전송 완료")
+            break
+        case MessageComposeResult.cancelled:
+            print("취소")
+            break
+        case MessageComposeResult.failed:
+            print("전송 실패")
+            break
+        @unknown default:
+            fatalError()
+        }
+        controller.dismiss(animated: true, completion: nil)
     }
 }
 
