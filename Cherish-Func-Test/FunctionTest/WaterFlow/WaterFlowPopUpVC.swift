920ef0b6cfa444b0fdbaf100b5b169467484056f
wonsuckl <wurijang@naver.com>
2021-01-02 02:45:54 +0900

    [fix]통화연결 후 후기 문제 해결

--- Cherish-Func-Test/FunctionTest/WaterFlow/WaterFlowPopUpVC.swift
+++ Cherish-Func-Test/FunctionTest/WaterFlow/WaterFlowPopUpVC.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 import MessageUI
+import CallKit
 
-class WaterFlowPopUpVC: UIViewController {
+class WaterFlowPopUpVC: UIViewController,CXCallObserverDelegate {
     var ct : FetchedContact?
+    let callObserver = CXCallObserver()
+    var didDetectOutgoingCall = false
     
     //MARK: -@IBOutlet
     @IBOutlet weak var popupView: UIView!{
@@ -37,23 +40,80 @@ class WaterFlowPopUpVC: UIViewController {
     @IBAction func backToFlow(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    /// Contact way1: Call
-    @IBAction func calling(_ sender: Any) {
-        guard let pvc = self.presentingViewController else {return}
-        if let url = NSURL(string: "tel://" + "\(ct?.telephone ?? "0")"),
-           UIApplication.shared.canOpenURL(url as URL) {
-            /// 연락 수단 선택 창 dismiss
-            self.dismiss(animated: true){
-                UIApplication.shared.open(url as URL, options: [:]){_ in
-                    let storyBoard: UIStoryboard = UIStoryboard(name: "Review", bundle: nil)
-                    if let vc = storyBoard.instantiateViewController(withIdentifier: "ReviewVC") as? ReviewVC{
-                        vc.modalPresentationStyle = .fullScreen
-                        pvc.present(vc, animated: true, completion: nil)
-                    }
+    
+    /// Call Material
+    func showCallAlert() {
+        guard let url = NSURL(string: "tel://" + "\(ct?.telephone ?? "0")"),
+            UIApplication.shared.canOpenURL(url as URL) else {
+                return
+        }
+
+        callObserver.setDelegate(self, queue: nil)
+
+        didDetectOutgoingCall = false
+
+        //we only want to add the observer after the alert is displayed,
+        //that's why we're using asyncAfter(deadline:)
+        UIApplication.shared.open(url as URL, options: [:]) { [weak self] success in
+            if success {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                    self?.addNotifObserver()
                 }
             }
         }
     }
+    /// Contact way1: Call
+    @IBAction func calling(_ sender: Any) {
+        showCallAlert()
+    }
+//        callObserver.setDelegate(self, queue: nil)
+//        didDetectOutgoingCall = false
+//
+//        //we only want to add the observer after the alert is displayed,
+//        //that's why we're using asyncAfter(deadline:)
+//        if let url = NSURL(string: "tel://" + "\(ct?.telephone ?? "0")"),
+//           UIApplication.shared.canOpenURL(url as URL) {
+//            /// 연락 수단 선택 창 dismiss
+//            self.dismiss(animated: true){
+//                UIApplication.shared.open(url as URL, options: [:]){ [weak self] success in
+//                    if success {
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+//                            self?.addNotifObserver()
+//                        }
+//                    }
+//                }
+//            }
+//        }
+    
+    
+    func addNotifObserver() {
+        let selector = #selector(appDidBecomeActive)
+        let notifName = UIApplication.didBecomeActiveNotification
+        NotificationCenter.default.addObserver(self, selector: selector, name: notifName, object: nil)
+    }
+    
+    @objc func appDidBecomeActive() {
+        //if callObserver(_:callChanged:) doesn't get called after a certain time,
+        //the call dialog was not shown - so the Cancel button was pressed
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { [weak self] in
+            if !(self?.didDetectOutgoingCall ?? true) {
+                print("Cancel button pressed")
+            }
+        }
+    }
+    
+    func callObserver(_ callObserver: CXCallObserver, callChanged call: CXCall) {
+        if call.isOutgoing && !didDetectOutgoingCall {
+            didDetectOutgoingCall = true
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Review", bundle: nil)
+            if let vc = storyBoard.instantiateViewController(withIdentifier: "ReviewVC") as? ReviewVC{
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true, completion: nil)
+            }
+            print("Call button pressed")
+        }
+    }
+    
     /// Contact way2: Message
     @IBAction func smsing(_ sender: Any) {
         //        guard let pvc = self.presentingViewController else {return}
