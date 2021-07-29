abf8069e6146815a33412ec1222deabdbd341ec6
wonseok <wurijang@gmail.com>
2021-07-30 00:21:31 +0900

    [add] GCD-practice

---
+++ GCD-practice/GCD-practice/main.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  GCD-practice
+//
+//  Created by Wonseok Lee on 2021/07/29.
+//
+
+import Foundation
+let wsQueue = DispatchQueue(label: "ws")
+
+// MARK: - Serial && sync
+//wsQueue.sync {
+//    for i in 1...5 {
+//            print(i)
+//        }
+//        print("==================")
+//}
+//
+//for i in 100...105 {
+//        print(i)
+//    }
+//    print("==================")
+
+// MARK: - Concurrent && sync
+//DispatchQueue.global().sync {
+//    for i in 1...5 {
+//            print(i)
+//        }
+//        print("==================")
+//}
+//
+//for i in 100...105 {
+//        print(i)
+//    }
+//    print("==================")
+
+
+// MARK: - Concurrent && async
+//DispatchQueue.global().async {
+//    for i in 1...5 {
+//        print("\(i)🚀")
+//    }
+//    print("==================")
+//}
+//DispatchQueue.global().async {
+//    for i in 200...205 {
+//        print("\(i)🥕")
+//    }
+//    print("==================")
+//}
+//
+//for i in 100...105 {
+//    print("\(i)👻")
+//}
+
+// MARK: - Serial && async
+wsQueue.async {
+    for i in 1...5 {
+        print("\(i)🚀")
+    }
+    print("==================")
+}
+wsQueue.async {
+    for i in 200...205 {
+        print("\(i)🥕")
+    }
+    print("==================")
+}
+
+for i in 100...105 {
+    print("\(i)👻")
+}
